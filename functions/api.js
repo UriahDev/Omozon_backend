02deb3b041928c92700c5d1d2808325b9ce8e96a
UriahDev <devuriah@gmail.com>
2022-07-01 14:23:41 +0000

    cors changes

--- functions/api.js
+++ functions/api.js
@@ -12,7 +12,7 @@ const app = express()
 app.use(express.json())
 app.use(cors({
     origin: 'https://omozon.netlify.app',
-    credentials: true
+    credentials: false
 }))
 
 const YOUR_DOMAIN = process.env.YOUR_DOMAIN
@@ -56,7 +56,7 @@ router.post('/create-checkout-session', async (req, res) => {
       cancel_url: `${YOUR_DOMAIN}?canceled=true`,
     })
     //res.status(303): task
-    res.header('Access-Control-Allow-Origin', 'https://omozon.netlify.app')
+    res.header('Access-Control-Allow-Origin', '*')
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT')
     res.json(session.url)
